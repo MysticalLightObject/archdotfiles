48a19ee2aed2fca736091d8e32b59ab7fec4a243
Michael Nikolaev <michael.nikolaev@piano.io>
2022-10-26 10:47:55 +0400

    New generation dotfiles

---
+++ install.sh
@@ -0,0 +1,9 @@
+# install oh-my-zsh
+# copy .zshrc to ~/.zshrc
+#
+# install nvm
+# copy nvim config (.config) and plugins (.local/nvim)
+# install jq & yq
+# install google-chrome
+# install neovim & make alias(?)
+# install amazon corretto
