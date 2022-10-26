0e75e7de9b739c6c05ae3f30ebe71a37d6ae4ccb
Michael Nikolaev <michael.nikolaev@piano.io>
2022-10-26 11:06:06 +0400

    Add i3

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
+# install base-devel
+# install yay
 # install oh-my-zsh
 # copy .zshrc to ~/.zshrc
-#
+
 # install nvm
 # copy nvim config (.config) and plugins (.local/nvim)
+
 # install jq & yq
+# install ripgrep
 # install google-chrome
 # install neovim & make alias(?)
 # install amazon corretto
+# install intelliJ IDEA
