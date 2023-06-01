e0f25cce7a2aa0a539f557b5e5a982253dc80c77
Michael Nikolaev <michael.nikolaev@piano.io>
2023-06-01 17:18:02 +0400

    Change font to Fira Code. Refactor install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~
-# install base-devel
-sudo pacman -Syu base-devel
+# install some packages
+sudo pacman -Syu base-devel nerd-fonts-terminus nerd-fonts-noto-sans-mono noto-fonts-cjk noto-fonts-emoji jq yq ttf-fira-code firefox ripgrep neovim pip3
 
 # install yay
 git clone https://aur.archlinux.org/yay-git.git
@@ -13,31 +13,12 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # TODO: copy config file 
 
 # copy .zshrc to ~/.zshrc
-# 
-# install nerdfonts
-# install Chinese fonts
-# install emoji fonts
-# install jq & yq
-sudo pacman -Syu nerd-fonts-terminus nerd-fonts-noto-sans-mono noto-fonts-cjk noto-fonts-emoji jq yq
 
-# install ripgrep
-sudo pacman -Syu ripgrep
 # install google-chrome
-yay google-chrome
-# install neovim & make alias(?)
-sudo pacman -Syu neovim
-# copy nvim config (.config) and plugins (.local/nvim)
-
-# install amazon corretto
-yay amazon-corretto-11
-# install intelliJ IDEA
-yay jetbrains-toolbox
+yay google-chrome amazon-corretto-11 jetbrains-toolbox
 
 # fix audio
 install_pulse
-
-#install pip3
-sudo pacman -Syu pip3
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
@@ -56,7 +37,6 @@ sudo pacman -S pritunl-client-electron
 # fix ntp ???
 systemctl status systemd-timesyncd
 
-# install aws
-# install kubectl
-# install screenshot utility
-yay -S escrotum-git
+# TODO: install aws
+# TODO: install kubectl
+# TODO: copy nvim config (.config) and plugins (.local/nvim)
